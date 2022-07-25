ff11b927611fba3666e9e5926f0afac6fbf4d7cd
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-24 21:27:42 -0400

    filter works

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        onChange={onSearchChange}
+        type="text"
+        name="search"
+        placeholder="Search..."
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,24 +5,38 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [text, setText] = useState('')
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+
+  const onSearchChange = (e) => {
+    setText(e.target.value)
+  }
+
+  // const itemsFiltered = items.filter((item) => {
+  //   // if (setText === '') return true;
+
+  //   return item.name === text;
+  // })
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  })
+    .filter((item) => item.name.toLowerCase().includes(text.toLowerCase()));
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onSearchChange={onSearchChange} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
+
         ))}
       </ul>
     </div>
